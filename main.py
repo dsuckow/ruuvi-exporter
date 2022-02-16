2121e6efa013ff9ff0e7bc6e640ba1b813e23b61
Daniel Suckow <dasuckow@gmail.com>
2022-02-16 21:25:50 +0100

    add infinite while and signal handler

--- main.py
+++ main.py
@@ -25,6 +25,8 @@ debug = False
 testdata = True
 testdata_file = "/home/pi/ruuvi-exporter/test/ruuvi-data-part.json"
 
+PID_FILENAME = "ruuvi-exporter.pid"
+
 class UpdateThread (threading.Thread):
     def __init__(self, threadID, name, counter):
         threading.Thread.__init__(self)
@@ -33,41 +35,44 @@ class UpdateThread (threading.Thread):
         self.counter = counter
     def run(self):
         logger.debug(f'start thread {self.name} to fetch ruuvi data in background and update the gauge')
-#        while True:
-        if beacons.keys():
-            logger.debug(f'fetch data for: {beacons.keys()}')
-            if not testdata:
-                logger.debug(f'fetch data from ruuvi tag library')
-                datas = RuuviTagSensor.get_data_for_sensors(beacons.keys(), 10)
-            else:
-                datas = {}
-                with open(testdata_file) as test_file:
-                    logger.debug(f'fetch data from {testdata_file}')
-                    datas = json.load(test_file)
-            data_response = pformat(datas)
-            logger.debug(f'data:\n{data_response}')
-            for key, value in datas.items() :
-                b = beacons[key]
-                location = b['name']
-                logger.debug(f'==== {key} == location: {location} ====')
-                sensor_data = datas[key]
-                temperature = sensor_data['temperature']
-                humidity = (sensor_data['humidity'] / 100.0)
-                pressure = sensor_data['pressure']
-                battery = (sensor_data['battery'] / 1000.0)
-                if temperature != 0:
-                    logger.debug(f"temperature: {temperature}")
-                    temp_gauge.labels(location).set(temperature)
-                if humidity != 0:
-                    logger.debug(f"humidity: {humidity}")
-                    humidity_gauge.labels(location).set(humidity)
-                if pressure != 0:
-                    logger.debug(f"pressure: {pressure}")
-                    pressure_gauge.labels(location).set(pressure)
-                if battery != 0:
-                    logger.debug(f"battery: {battery}")
-                    battery_gauge.labels(location).set(battery)
-    #            time.sleep(update_delay)
+        while True:
+            if beacons.keys():
+                logger.debug(f'fetch data for: {beacons.keys()}')
+                if not testdata:
+                    logger.debug(f'fetch data from ruuvi tag library')
+                    datas = RuuviTagSensor.get_data_for_sensors(beacons.keys(), 10)
+                else:
+                    datas = {}
+                    with open(testdata_file) as test_file:
+                        logger.debug(f'fetch data from {testdata_file}')
+                        datas = json.load(test_file)
+                parse_data(datas)
+            time.sleep(update_delay)
+
+def parse_data(data):
+    data_response = pformat(data)
+    logger.debug(f'data:\n{data_response}')
+    for key, value in data.items() :
+        b = beacons[key]
+        location = b['name']
+        logger.debug(f'==== {key} == location: {location} ====')
+        sensor_data = data[key]
+        temperature = sensor_data['temperature']
+        humidity = (sensor_data['humidity'] / 100.0)
+        pressure = sensor_data['pressure']
+        battery = (sensor_data['battery'] / 1000.0)
+        if temperature != 0:
+            logger.debug(f"temperature: {temperature}")
+            temp_gauge.labels(location).set(temperature)
+        if humidity != 0:
+            logger.debug(f"humidity: {humidity}")
+            humidity_gauge.labels(location).set(humidity)
+        if pressure != 0:
+            logger.debug(f"pressure: {pressure}")
+            pressure_gauge.labels(location).set(pressure)
+        if battery != 0:
+            logger.debug(f"battery: {battery}")
+            battery_gauge.labels(location).set(battery)
 
 def start_ruuvi_update_thread():
     thread = UpdateThread(1, "Ruuvi-Update-Thread", 1)
@@ -87,11 +92,6 @@ def setup_logging():
     logging.basicConfig(format='%(asctime)s %(levelname)-9s %(message)s', level=logging.WARNING)
     global logger
     logger = logging.getLogger(__name__)
-    # logger.critical(f'test critical')
-    # logger.error(f'test error')
-    # logger.warning(f'test warning')
-    # logger.info(f'test info')
-    # logger.debug(f'test debug')
 
 def log_config_and_process():
     config = pformat(beacons)
@@ -99,7 +99,7 @@ def log_config_and_process():
     pid = os.getpid()
     logger.debug(f'pid: {pid}')
     try:
-        with open("ruuvi-exporter.pid", "x") as pid_file:
+        with open(PID_FILENAME, "x") as pid_file:
             pid_file.write(str(pid))
     except OSError as e:
         logger.error(f'can\'t open ruuvi-exporter.pid: {e}, process is either running or has a stale pid file!')
@@ -109,8 +109,19 @@ def start_metrics_server():
     logger.debug(f'Starting HTTP server on port {port} for Prometheus scraping')
     start_http_server(port)
 
+def sigterm_handler(sig, frame):
+    logger.info(f'receive signal {sig} on {frame}')
+    if os.path.exists(PID_FILENAME):
+        os.remove(PID_FILENAME)
+    sys.exit(0)
+
+def init_signal_handler():
+    signal.signal(signal.SIGINT, sigterm_handler)
+    signal.signal(signal.SIGTERM, sigterm_handler)
+
 def main():
     setup_logging()
+    init_signal_handler()
     parse_args()
     load_config()
     log_config_and_process()
