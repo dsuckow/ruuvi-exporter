586980c45afd61a92202c117208793286277d638
Daniel Suckow <dasuckow@gmail.com>
2022-02-16 09:50:35 +0100

    change logging and refactoring

--- main.py
+++ main.py
@@ -6,6 +6,8 @@ import signal
 import sys
 import json
 import threading
+import logging
+from pprint import pformat
 
 from prometheus_client import Gauge, start_http_server
 from ruuvitag_sensor.ruuvi import RuuviTagSensor
@@ -18,6 +20,7 @@ update_delay = 30
 port = 8000
 beacons = None
 config_file = "/home/pi/ruuvi-exporter/config.json"
+debug = False
 
 class updateThread (threading.Thread):
     def __init__(self, threadID, name, counter):
@@ -26,42 +29,66 @@ class updateThread (threading.Thread):
         self.name = name
         self.counter = counter
     def run(self):
-        print("update thread started")
+        logger.debug(f'start thread {self.name} to fetch ruuvi data in background and update the gauge')
 #        while True:
         if beacons.keys():
-            print("fetch data for: "+str(beacons.keys()))
+            logger.debug(f'fetch data for: {beacons.keys()}')
             datas = RuuviTagSensor.get_data_for_sensors(beacons.keys(), 10)
-            print("data: "+str(datas))
+            logger.debug(f'data: {datas}')
             for key, value in datas.items() :
-                print("=== " + key + " ===")
+                logger.debug(f'=== {key} ===')
                 b = beacons[key]
                 location = b['name']
-                print("location: "+location)
+                logger.debug(f'location: {location}')
                 sensor_data = datas[key]
-                print("temperature: "+str(sensor_data['temperature']))
-                print("humidity: "+str(sensor_data['humidity'] / 100.0))
-                print("pressure: "+str(sensor_data['pressure']))
-                print("battery: "+str(sensor_data['battery'] / 1000.0))
+                logger.debug(f'temperature: {sensor_data['temperature']}')
+                logger.debug(f'humidity: {(sensor_data['humidity'] / 100.0)}')
+                logger.debug(f'pressure: {sensor_data['pressure']}')
+                logger.debug(f'battery: {(sensor_data['battery'] / 1000.0)}')
                 temp_gauge.labels(location).set(sensor_data['temperature'])
                 humidity_gauge.labels(location).set(sensor_data['humidity'] / 100.0)
                 pressure_gauge.labels(location).set(sensor_data['pressure'])
                 battery_gauge.labels(location).set(sensor_data['battery'] / 1000.0)
     #            time.sleep(update_delay)
 
+def start_ruuvi_update_thread():
+    thread = updateThread(1, "Ruuvi-Update-Thread", 1)
+    thread.start()
+
 def load_config():
     with open(config_file) as json_file:
         global beacons
         beacons = json.load(json_file)
 
-def main():
-    load_config()
-    print("beacons: ", end="")
-    print(beacons)
-#    print("Starting HTTP server for Prometheus scraping")
-#    start_http_server(port)
+def parse_args():
+    logger.debug(f'parse args')
+    if True:
+        logger.setLevel(logging.DEBUG)
 
-    thread = updateThread(1, "Ruuvi-Update-Thread", 1)
-    thread.start()
+def setup_logging():
+    logging.basicConfig(format='%(asctime)s %(levelname)-9s %(message)s', level=logging.WARNING)
+    global logger
+    logger = logging.getLogger(__name__)
+    # logger.critical(f'test critical')
+    # logger.error(f'test error')
+    # logger.warning(f'test warning')
+    # logger.info(f'test info')
+    # logger.debug(f'test debug')
+
+def log_config():
+    config = pformat(beacons)
+    logger.debug(f'config: {config}')
+
+def start_server():
+    logger.debug(f'Starting HTTP server on port {port} for Prometheus scraping')
+    start_http_server(port)
+
+def main():
+    setup_logging()
+    parse_args()
+    load_config()
+    log_config()
+    start_ruuvi_update_thread()
 
 if __name__ == '__main__':
     main()
